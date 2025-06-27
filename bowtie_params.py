bcb1e2f5b92fa45f26324f8621b75426cdfd946e
Ben Moginot <bmoginot5@gmail.com>
2025-06-27 10:46:38 -0500

    run bowtie with different parameters:

---
+++ bowtie_params.py
@@ -0,0 +1,61 @@
+"""run bowtie2 on the same read pair with different parameters to compare stringency"""
+
+import sys
+import glob
+import argparse
+import subprocess
+
+def get_args(args):
+    """retrieve command line arguements"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--reads", required=True)
+    parser.add_argument("-m", "--mag", required=True)
+    parser.add_argument("-t", "--threads")
+    return parser.parse_args(args)
+
+def run_bowtie(mag, ind, reads, threads):
+    print("building index...")
+
+    subprocess.run([ # build index to act as reference for bt2
+        "bowtie2-build", "-q",
+        mag,
+        ind
+    ])
+
+    print(f"done\n")
+
+    proc = str(threads) if threads else "1"
+
+    sample_num = reads[0].split("/")[-1].split("_")[0]
+    fread = reads[0]
+    rread = reads[1]
+
+    params = ["--very-fast", "--fast", "--sensitive", "--very-sensitive"]
+
+    for opt in params:
+        print(f"running bowtie2 with {opt}...")
+
+        subprocess.run([
+            "bowtie2",
+            opt, # parameter affecting stringency
+            "-p", proc,
+            "-x", ind,
+            "-1", fread,
+            "-2", rread,
+            "-S" f"{sample_num}_map.sam",
+            "--al-conc", f"{sample_num}_%.fq"
+        ])
+        
+        print(f"done\n")
+
+def main():
+    args = get_args(sys.argv[1:])
+
+    reads = sorted(glob.glob(f"{args.reads}/*"))
+
+    bladder_mag = args.mag
+    bt2_index = "pat17_bladder"
+    run_bowtie(bladder_mag, bt2_index, reads, args.threads)
+
+if __name__ == "__main__":
+    main()
