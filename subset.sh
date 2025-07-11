57cf920c8fd69a7a3b0785c6e7c1127d44528af9
Ben Moginot <bmoginot5@gmail.com>
2025-07-11 11:43:26 -0500

    cleaning up tmp scripts

--- subset.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for f in data/pat17/*
-do
-    filename=$(basename $f) # get just the name of the file, not the whole path
-    out=${filename/fastq.gz/fq}
-    zless $f | head -n 40000 > $out
-done
